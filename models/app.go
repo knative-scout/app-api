3b2d89be0775b3537dc7bd8dad853896cad719e0
Noah Huppert <contact@noahh.io>
2019-06-03 13:30:39 -0400

    added app model

---
+++ models/app.go
@@ -0,0 +1,41 @@
+package models
+
+// App is a serverless application from the repository
+type App struct {
+	// ID is a human and computer readable identifier for the application
+	ID string `json:"id"`
+
+	// Name to display to users
+	Name string `json:"name"`
+
+	// Tagline is a short description of the app
+	Tagline string `json:"tagline"`
+
+	// Description is more detailed markdown formatted information about the app
+	Description string `json:"description"`
+
+	// ScreenshotURLs are links to app screenshots
+	ScreenshotURLs []string `json:"screenshot_urls"`
+
+	// LogoURL is a link to the app logo
+	LogoURL string `json:"logo_url"`
+
+	// Tags are lists of tags
+	Tags []string `json:"tags"`
+
+	// VerificationStatus indicates the stage of the verification process the app
+	// is currently in. Can be one of: "pending", "verifying", "good", "bad"
+	VerificationStatus string `json:"verification_status"`
+
+	// GitHubURL is a link to the GitHub files for the app
+	GitHubURL string `json:"github_url"`
+
+	// Version is the semantic version of the app
+	Verison string `json:"version"`
+
+	// Author is the person who created the app
+	Author string `json:"author"`
+
+	// Maintainer is the person who will support the app
+	Maintainer string `json:"maintainer"`
+}
