9626a9bc3a66214173d832510d9a6c3b02a459b0
pratikmahajan <pratik@mahajan.xyz>
2019-06-05 20:49:30 -0400

    apps tags working commit

--- handlers/apptags.go
+++ handlers/apptags.go
@@ -6,8 +6,14 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"net/http"
+	"strings"
+
+	//"strings"
 )
 
+
+// AppTagsHandler is used to get all the tags stored in the database,
+// can also be used to get tags of matched apps if query is provided
 type AppTagsHandler struct {
 	BaseHandler
 }
@@ -15,6 +21,7 @@ type AppTagsHandler struct {
 // ServeHTTP implements http.Handler
 func (h AppTagsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
+	//gets all the optional parameters passed in the URL
 	vars := r.URL.Query()
 	query := vars.Get("query")
 
@@ -24,7 +31,6 @@ func (h AppTagsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		"tags":result,
 	}
 
-
 	h.RespondJSON(w, http.StatusOK, resp)
 }
 
@@ -33,28 +39,46 @@ func getTagsFromDB(query string, h AppTagsHandler ) []string {
 
 	tagsBson := bson.D{}
 
-
+	// if we have additional parametres, only get the tags of matched apps.
 	if len(query)>0{
+		query := strings.Split(query, ",")
 		tagsBson = append(tagsBson, bson.E{
-			"tags", query,
-			//bson.D{{"$in", query}},
+			"tags",
+			bson.D{{"$in", query}},
 		})
 	}
 
-	result, err := h.MDbApps.Find(h.Ctx, tagsBson, options.Find().SetProjection(bson.E{"tags",1}) )
+	// Declaring structure to set projection in find. used to single out the column. for more info see *findoptions in mongo
+	type fields struct {
+		Tags int `bson:"tags"`
+	}
+
+	// Declaring structure to decode the output from the mongo tags search query
+	type tagsRes struct{
+		Tags []string `json:"tags" bson:"tags"`
+	}
+
+	// setting projection
+	projection := fields{
+		Tags: 1,
+	}
+
+	result, err := h.MDbApps.Find(h.Ctx, tagsBson, options.Find().SetProjection(projection) )
 	if err != nil {
 		panic(fmt.Errorf("unable to query database", err.Error()))
 	}
 
+	// declaring return string
+	ret := []string{}
+
 	for result.Next(h.Ctx) {
-		a := []string{}
+		a := tagsRes{}
 		if err = result.Decode(&a); err != nil {
 			panic(fmt.Errorf("Error in decode %s", err.Error()))
 		}
-		h.Logger.Debugf("take this: %#T", a)
+		ret= append(ret, a.Tags...)
+
 	}
 
-
-
-	return []string{"lala", "kaka"}
+	return ret
 }
