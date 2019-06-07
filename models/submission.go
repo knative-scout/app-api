835a4007dc603a66ebc4729e45beb91a129f53d8
Noah Huppert <contact@noahh.io>
2019-06-07 17:17:18 -0400

    made Submission.Apps a map

--- models/submission.go
+++ models/submission.go
@@ -7,8 +7,11 @@ type Submission struct {
 	// PRNumber is the user facing ID number of the pull request
 	PRNumber int `bson:"pr_number"`
 
-	// Apps are the applications which are currently in the pull request
-	Apps []SubmissionApp `bson:"apps"`
+	// Apps are the applications which are currently in the pull request.
+	// Keys are app IDs.
+	// A value can be nil if an internal system error occurred while parsing /
+	// loading the app.
+	Apps map[string]*SubmissionApp `bson:"apps"`
 }
 
 // SubmissionApp associates an app in a submission with a verification status entry
@@ -18,11 +21,11 @@ type SubmissionApp struct {
 	App *App `bson:"app"`
 
 	// VerificationStatus of the App
-	VerificationStatus VerificationStatus `bson:"verification_status"`
+	VerificationStatus AppVerificationStatus `bson:"verification_status"`
 }
 
-// VerificationStatus holds the verification status of an app
-type VerificationStatus struct {
+// AppVerificationStatus holds the verification status of an app
+type AppVerificationStatus struct {
 	// FormatCorrect indicates if the app submission files are formatted correctly
 	FormatCorrect bool `bson:"format_correct"`
 }
