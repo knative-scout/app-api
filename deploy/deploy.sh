0af1f7a8cd99969e2d629fc3827690f9b7733e21
Noah Huppert <contact@noahh.io>
2019-06-10 13:55:00 -0400

    made it possible to deploy apps to different envs

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,43 +1,47 @@
 #!/usr/bin/env bash
 #?
-# deploy.sh - Manage deployments
+# deploy.sh - Deploy API and database
 #
 # USAGE
 #
-#    deploy.sh CMD COMPONENT
+#    deploy.sh [-e ENV] [-h HOST] [CMD]
+#
+# OPTIONS
+#
+#    -s SECRETS_F    File which contains secret data, see SECRETS section for required keys
+#    -e ENV     (Optional) Deployment environment, defaults to "prod"
+#    -h HOST    (Optional) API host. If ENV == prod defaults to "api.kscout.io".
+#                          If ENV is anything else defaults to "${ENV}-api.kscout.io"
 #
 # ARGUMENTS
 #
-#    CMD          Action to complete, valid values: up, down
-#    COMPONENT    Part of deployment to manage, valid values: app, db, all
+#    CMD    (Optional) Kubectl command, defaults to "apply"
+#
+# SECRETS
+#
+#    Secrets will be provided via JSON / YAML / TOML file with the following structure:
+#
+#    - mongo (Object): MongoDB secrets
+#      - password (String): API user password
+#    - github (Object): GitHub secrets
+#      - apiToken (String): API token with which to make authenticated API calls
+#      - webhookSecret (String): Secret GitHub uses to sign an HMAC of requests sent
+#                                to the API webhook endpoint
 #
 # BEHAVIOR
 #
-#    The up command deploys resources, the down command destroyes resources.
+#    Runs gomplate (https://docs.gomplate.ca/) over the resource files in the
+#    deploy/templates directory. Resource files can use Go templating. The following
+#    keys will hold data:
 #
-#    The app component refers to the API server.
-#    The db component refers to the MongoDB server.
-#    The all component refers to all of the above.
+#    - .config: Will hold values of -e and -h options in object with keys "env" and "host"
+#    - .secrets: Holds secret values, see SECRETS section for structure of object
+#
+#    Deploys the resulting YAML to Kubernetes under the kscout namespace.
 #
 #?
 
-# {{{1 Configuration
-prog_dir=$(realpath $(dirname "$0"))
-
-if [ -z "$kubectl" ]; then
-    kubectl="$prog_dir/../tmpk"
-fi
-
-common_file_args=(
-    "--filename" "$prog_dir/ns.yaml"
-    "--filename" "$prog_dir/secrets.yaml"
-)
-app_file_args=(
-    "--filename" "$prog_dir/app.yaml"
-)
-db_file_args=(
-    "--filename" "$prog_dir/db.yaml"
-)
+prog_dir=$(realpath $(dirname "$0")) 
 
 # {{{1 Helpers
 function die() {
@@ -49,32 +53,112 @@ function bold() {
     echo "$(tput bold)$@$(tput sgr0)"
 }
 
-# {{{1 Arguments
-cmd="$1"
-component="$2"
+# {{{1 Options
+# {{{2 Defaults
+env=prod
+host=api.kscout.io
 
-case "$cmd" in
-    up)
-	kubectl_cmd=apply
-	file_args=("${common_file_args[@]}")
-	;;
-    down) kubectl_cmd=delete ;;
-    *) die "CMD arugment must be \"up\" or \"down\"" ;;
-esac
+# {{{2 Get
+while getopts "s:e:h:" opt; do
+    case "$opt" in
+	e)
+	    env="$OPTARG"
+	    shift
+	    shift
+	    ;;
+	h)
+	    host="$OPTARG"
+	    custom_host=true
+	    shift
+	    shift
+	    ;;
+	s)
+	    secrets_file="$(realpath $OPTARG)"
+	    shift
+	    shift
+	    ;;
+	'?') die "Unknown option" ;;
+    esac
+done
 
-case "$component" in
-    app) file_args+=("${app_file_args[@]}") ;;
-    db) file_args+=("${db_file_args[@]}") ;;
-    all) file_args+=("${app_file_args[@]}" "${db_file_args[@]}") ;;
-    *) die "COMPONENT arugment must be \"app\", \"db\" or \"all\"" ;;
-esac
-
-# {{{1 Run
-bold "Bringing $component $cmd"
-
-# {{{2 Manage resources
-if ! $kubectl "$kubectl_cmd" "${file_args[@]}"; then
-    die "Failed to bring $component $cmd"
+# {{{2 Computed defaults
+if [ -z "$custom_host" ] && [[ "$env" != "prod" ]]; then
+    host="$env-api.kscout.io"
 fi
 
-bold "Success"
+# {{{2 Ensure -s option provided
+if [ -z "$secrets_file" ]; then
+    die "-s SECRETS_F option must be provided"
+fi
+
+if [ ! -f "$secrets_file" ]; then
+    die "-s $secrets_file file does not exist"
+fi
+
+secrets_file_ext=${secrets_file##*.}
+
+# {{{1 Arguments
+cmd="$1"
+
+if [ -z "$cmd" ]; then
+    cmd=apply
+fi
+
+# {{{1 Print run parameters
+bold "Configuration"
+echo "env : $env"
+echo "host: $host"
+
+# {{{1 Bake templates
+bold "Baking resource templates"
+# {{{2 Save input option data as file
+sha=$(echo "$data_file_contents" | sha256sum | awk '{ print $1 }')
+
+data_file_contents="{\"env\": \"$env\", \"host\": \"$host\"}"
+data_file="/tmp/$sha.json"
+
+out_dir="/tmp/$sha-out"
+
+mkdir -p "$out_dir"
+echo "$data_file_contents" > "$data_file"
+
+function cleanup() {
+    if [ -f "$data_file" ]; then
+	if ! rm "$data_file"; then
+	    die "Failed to delete data file: $data_file"
+	fi
+    fi
+
+    if [ -d "$out_dir" ]; then
+	if ! rm -rf "$out_dir"; then
+	    die "Failed to delete out dir: $out_dir"
+	fi
+    fi
+}
+
+trap cleanup EXIT
+
+# {{{2 Run
+docker run \
+       -it \
+       --rm \
+       -v "$prog_dir/templates:/in" \
+       -v "$out_dir:/out" \
+       -v "$data_file:/tmp/data-file.json" \
+       -v "$secrets_file:/tmp/secrets-file.$secrets_file_ext" \
+       noahhuppert/gomplate:dev \
+       --input-dir /in \
+       --output-dir /out \
+       -c config=/tmp/data-file.json \
+       -c secrets=/tmp/secrets-file."$secrets_file_ext"
+
+if [[ "$?" != "0" ]]; then
+    die "Failed to bake templates"
+fi
+
+# {{{2 Deploy
+bold "kubectl -n kscout $cmd"
+for file in $(ls "$out_dir"); do
+    cat "$out_dir/$file" | kubectl -n kscout "$cmd" --filename -
+done
+
