b7348bc0a7d54a17aecf926db12912efb6db6b7e
pratikmahajan <pratik@mahajan.xyz>
2019-08-05 10:35:20 -0400

    nsearch standardization

--- handlers/smartsearch.go
+++ handlers/smartsearch.go
@@ -7,6 +7,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"net/http"
+	"strings"
 )
 
 // SmartSearchHandler is used search apps and return result
@@ -21,11 +22,15 @@ func (h SmartSearchHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	//gets all the optional parameters passed in the URL
 	vars := r.URL.Query()
 	query := vars.Get("query")
+	tags := vars.Get("tags")
+	categories := vars.Get("categories")
 
-	result := smartSearchDB(query, h)
+	apps, categsRes, tagsRes := smartSearchDB(query, tags, categories, h)
 
-	resp := map[string][]models.App{
-		"apps":result,
+	resp := map[string]interface{}{
+		"apps":apps,
+		"categories":categsRes,
+		"tags":tagsRes,
 	}
 
 
@@ -33,7 +38,7 @@ func (h SmartSearchHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 
-func smartSearchDB(query string,  h SmartSearchHandler ) []models.App{
+func smartSearchDB(query string, tags string, categories string, h SmartSearchHandler ) ([]models.App, []string, []string) {
 
 	// else, construct a bson query will all the required parameters and find in database
 	searchBson := bson.D{}
@@ -43,7 +48,20 @@ func smartSearchDB(query string,  h SmartSearchHandler ) []models.App{
 			bson.D{{"$search", query}},
 		})
 	}
-
+	if len(tags)>0{
+		tags := strings.Split(tags, ",")
+		searchBson = append(searchBson, bson.E{
+			"tags",
+			bson.D{{"$in", tags}},
+		})
+	}
+	if len(categories)>0{
+		categories := strings.Split(categories, ",")
+		searchBson = append(searchBson, bson.E{
+			"categories", bson.D{
+				{"$in", categories}},
+		})
+	}
 
 	// struct to set projection in mongo
 	type fields struct {
@@ -62,6 +80,9 @@ func smartSearchDB(query string,  h SmartSearchHandler ) []models.App{
 	findOptions.SetProjection(projection)
 
 	ret := []models.App{}  //to store all result as an array of json files
+	categsRes := []string{}
+	tagsRes := []string{}
+
 	result, err := h.MDbApps.Find(h.Ctx, searchBson , findOptions)
 
 	if err != nil {
@@ -74,6 +95,8 @@ func smartSearchDB(query string,  h SmartSearchHandler ) []models.App{
 			panic(fmt.Errorf("Unable to get apps from database: %s", err.Error()))
 		}
 		ret = append(ret,a)
+		categsRes = append(categsRes, a.Categories...)
+		tagsRes = append(tagsRes, a.Tags...)
 	}
-	return ret
+	return ret, removeDuplicates(categsRes), removeDuplicates(tagsRes)
 }
